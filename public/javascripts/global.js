bb4b63177269ec8e8f0ba7a5daecc6ce92b8fcde
Luisa <luisa@ydadmins-MBP.fritz.box>
2015-11-14 23:24:22 +0100

    add new questions and jquery functionality

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,13 +1,16 @@
 var answer=$('#the_answer').val(); 
+var not_picked_yet = true; 
 
 function check_answer(self){
     var option_chosen = self.innerText;
-    if(answer===option_chosen){
-        $(self).css('background-color', '#2ECC40');
-    } else {
-        $(self).css('background-color', '#FF4136');
-    }
+    if(answer!==option_chosen){
+        $(self).css('background-color', '#FF4136'); // red
+        $('#the_answer').css('background-color', '#FF4136'); // red
+    }else{
+        $(self).css('background-color', '#2ECC40'); // green
+    } 
     $(self).css('color', '#fff');
+    not_picked_yet = false; 
 };
 
 function next_question(){
@@ -27,19 +30,22 @@ function next_question(){
     });
 }
 
+function answer_action(self){
+    if(not_picked_yet){
+        check_answer(self);
+        setTimeout(next_question, 1800);
+    }
+}
+
 $('#choose_option1').click(function(e){
-    check_answer(this);
-    setTimeout(next_question, 1800);
+    answer_action(this);
 });
 $('#choose_option2').click(function(e){
-    check_answer(this);
-    setTimeout(next_question, 1800);
+    answer_action(this);
 });
 $('#choose_option3').click(function(e){
-    check_answer(this);
-    setTimeout(next_question, 1800);
+    answer_action(this);
 });
 $('#choose_option4').click(function(e){
-    check_answer(this);
-    setTimeout(next_question, 1800);
+    answer_action(this);
 });
